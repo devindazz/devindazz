f5c731457dfdbef594896ea7b1dc141af681497f
Devinda Wijesinghe <148736108+devindazz@users.noreply.github.com>
2025-03-19 22:30:46 +0530

    feat: add project page and new images to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/hero-section"
 import Aboutme from "@/components/about-me"
+import ProjectsPage from "@/components/project-page"
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <Aboutme/>
+      <ProjectsPage/>
       {/* Add other sections of your portfolio below */}
     </main>
   )
