a3ca7821591069f29671c069081a7ea276e3081d
snelusha <hello@snelusha.dev>
2025-04-27 23:16:43 +0530

    fix: correct casing of scroll down text

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -332,7 +332,7 @@ export default function Home() {
                   ease: "easeInOut",
                 }}
               >
-                scroll down
+                Scroll Down
               </motion.span>
             )}
 
