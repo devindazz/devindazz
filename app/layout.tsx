b26d4dc2e8d06f016d69f4f514294fbd6da44589
snelusha <hello@snelusha.dev>
2025-04-27 23:15:18 +0530

    feat: add viewport meta tag for dark color scheme

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,15 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
   title: "Devinda Wijesinghe",
   description: "Personal Portfolio of Devinda - A Software Engineer Student",
+};
+
+export const viewport: Viewport = {
+  colorScheme: "dark",
 };
 
 export default function RootLayout({
